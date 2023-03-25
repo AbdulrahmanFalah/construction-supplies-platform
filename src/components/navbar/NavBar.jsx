2e6e54f4b46c693041d7f4a115ac9833f21fb72e
Abdulrahman Falah <78597731+AbdulrahmanFalah@users.noreply.github.com>
2023-03-25 13:52:33 +0300

    search bar working

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-
 import "./NavBar.css";
-import serachIcon from "..//../assets/search-icon.svg";
-import cart from "..//../assets/cart.svg";
-import arrow from "..//../assets/arrow.svg";
+import searchIcon from "../../assets/search-icon.svg";
+import cart from "../../assets/cart.svg";
+import arrow from "../../assets/arrow.svg";
+import axios from "axios";
+
 function Navbar() {
+  const [search, setSearch] = useState("");
+  const handelCkick = () => {
+    axios
+      .get(`https://sikkala.onrender.com/api/search?query=${search}`)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <>
       <div className="navbar">
@@ -27,18 +40,21 @@ function Navbar() {
             </Link>
           </div>
           <div className="navbar__search navbar__search-web ">
-            <span>
-              <img
-                src={serachIcon}
-                alt="search icon"
-                className="search__icon"
-              ></img>
-            </span>
             <input
               type="text"
               className="navbar__search__input"
               placeholder="البحث عن مواد"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
             />
+            <span>
+              <img
+                src={searchIcon}
+                alt="search icon"
+                className="search__icon"
+                onClick={handelCkick}
+              ></img>
+            </span>
           </div>
 
           <div className="navbar__logo">
@@ -51,7 +67,7 @@ function Navbar() {
           <div className="navbar__search navbar__search-mobile">
             <span>
               <img
-                src={serachIcon}
+                src={searchIcon}
                 alt="search icon"
                 className="search__icon"
               ></img>
@@ -60,6 +76,8 @@ function Navbar() {
               type="text"
               className="navbar__search__input mobile-input"
               placeholder="البحث عن مواد"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
             />
           </div>
         </div>
@@ -67,4 +85,5 @@ function Navbar() {
     </>
   );
 }
+
 export default Navbar;
